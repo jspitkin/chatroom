122762099fd8d3943e5e416be99427531a698e84
Jake Pitkin <jakepitkin@gmail.com>
2016-01-07 16:27:43 -0700

    more things

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ io.on('connection', function(socket) {
 	socket.on('chat message', function(msg) {
 		io.emit('chat message', msg);
 	});
+	socket.on('disconnect', function(user){
+    	io.emit('user disconnect', user);
+  	});	
 })
 
 /* Listen for connections on port 3000 */
--- public/js/message.js
+++ public/js/message.js
@@ -1,16 +1,55 @@
 var socket = io();
+var username;
+var usernameSet = false;
 
 /* Sends the message to each connected user when form is submitted. */
-$('form').submit(function(){
+$('#sendForm').submit(function() {
 	socket.emit('chat message', $('#send').val());
     $('#send').val('');
     return false;
 });
 
+$('#userForm').submit(function() {
+	
+	return false;
+});
+
 /* Capture the emitted message from the server and display it. */
 socket.on('chat message', function(msg) {
-	if (msg !== '') {
-		$('#messages').append($('<li id="message">').text(msg));
+	if (msg !== '' && usernameSet) {
+		$('#messages').append($('<li id="message">').text(username + ": " + msg));
 		$("#chatWindow").scrollTop($("#chatWindow")[0].scrollHeight);
+	} 
+	else if (!usernameSet) {
+		$('#send').val('who are you lol');
 	}
 });
+
+
+
+socket.on('user disconnect', function(user) {
+	$('#messages').append($('<li id="message">').text(user + " has left."));
+});
+
+$('#sendName').on('click', function() {
+	username = $('#name').val().trim();
+	validUsername();
+});
+
+/* validates username entry */
+function validUsername() {
+	if (username !== '' && username.length < 8 && !usernameSet) {
+		$('#users').append($('<li id="user">').text(username));
+		$('#name').val('');
+		usernameSet = true;
+	} 
+	else if (username.length < 1) {
+		$('#name').val('');
+	}
+	else if (usernameSet) {
+		$('#name').val('no redos lol');
+	}
+	else {
+		$('#name').val('8 char limit lol');
+	}
+}
